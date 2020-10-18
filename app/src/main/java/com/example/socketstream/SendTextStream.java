13bd6e69868bd5e476de348a76942c9ca7b186dd
Aman <amanhack08@gmail.com>
2020-10-18 11:30:59 +0530

    get text message which you want to send

--- app/src/main/java/com/example/socketstream/SendTextStream.java
+++ app/src/main/java/com/example/socketstream/SendTextStream.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,12 +19,20 @@ public class SendTextStream extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_text_stream_activity);
         inputText=(EditText)findViewById(R.id.text_stream_input);
+        final String inputMessage=inputText.getText().toString().trim();
         sendButton=(Button)findViewById(R.id.send_button_text_stream);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(SendTextStream.this,PermissionRequiredTransfer.class);
-                startActivity(intent);
+                if(inputMessage!=null) {
+
+                    intent.putExtra("message",inputMessage);
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(SendTextStream.this, "Please enter text", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
