36471e6fe63392d6071282e62f4cb1bdcd4e0aab
Aman <amanhack08@gmail.com>
2020-10-20 00:52:45 +0530

    create a loader

--- app/src/main/java/com/example/socketstream/SendMediaStorage.java
+++ app/src/main/java/com/example/socketstream/SendMediaStorage.java
@@ -1,6 +1,7 @@
 package com.example.socketstream;
 
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
@@ -19,16 +20,29 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.socketstream.recyclerView.SendSongsRecyclerView;
 
 public class SendMediaStorage extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    //fragment view
     View v;
+
+    //make a recycler view instance
     SendSongsRecyclerView sendMediaStorage;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
          v=inflater.inflate(R.layout.send_media_storage,container,false);
+
+         //create an instance of recycler view
          sendMediaStorage=new SendSongsRecyclerView(getActivity());
+
+         //intialize the loader
          getLoaderManager().initLoader(0,null,this);
+
+         //create a recycler view object
         RecyclerView recyclerView=(RecyclerView)v.findViewById(R.id.send_media_recycler_view);
+
+        //set layout to recycler view
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+
+        //set adapter to recycler view
         recyclerView.setAdapter(sendMediaStorage);
          return v;
     }
@@ -37,6 +51,7 @@ public class SendMediaStorage extends Fragment implements LoaderManager.LoaderCa
     @Override
     public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
 
+        //using the concept of asynck task loader
         return new CursorLoader(getActivity().getApplicationContext()
                 ,MediaStore.Audio.Media.getContentUri("external")
                 ,null
@@ -47,11 +62,13 @@ public class SendMediaStorage extends Fragment implements LoaderManager.LoaderCa
 
     @Override
     public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
         sendMediaStorage.changeCursor(data);
     }
 
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        //when cursor changes
         sendMediaStorage.changeCursor(null);
     }
 }
