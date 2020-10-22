55fcb6ed9f2554d375793ae46950dc851ec2d470
Aman <amanhack08@gmail.com>
2020-10-22 10:03:26 +0530

    sender,client and sendreceive thread created

--- app/src/main/java/com/example/socketstream/DiscoverPeers.java
+++ app/src/main/java/com/example/socketstream/DiscoverPeers.java
@@ -1,19 +1,35 @@
 package com.example.socketstream;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +43,12 @@ public class DiscoverPeers extends AppCompatActivity {
     WifiP2pDevice[] deviceArray;
     DiscoversPeersBroadcastReciever discoversPeersBroadcastReciever;
     IntentFilter intentFilter;
+    static final int MESSAGE_READ=1;
+    ServerClass serverClass;
+    SendReceive sendReceive;
+    ClientClass clientClass;
+    String getDataFromPreviousActivity=null;
+    Button sendTextMessage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,15 +58,49 @@ public class DiscoverPeers extends AppCompatActivity {
         wifiP2pManager=(WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel=wifiP2pManager.initialize(this,getMainLooper(),null);
         peers=new ArrayList<>();
+        discoverPeers();
+        sendTextMessage=(Button)findViewById(R.id.send_button_discover_peers);
+        sendTextMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(getDataFromPreviousActivity!=null) {
+                    final String mesg =getDataFromPreviousActivity;
+                    Runnable runnable=new Runnable() {
+                        @Override
+                        public void run() {
+                            sendReceive.write(mesg.getBytes());
+                        }
+                    };
+
+                    Thread thread=new Thread(runnable);
+                    thread.start();
+
+                }
+            }
+        });
+
+        Intent intent=getIntent();
+        if(intent.getData()!=null&&intent.getData().toString().length()!=0)
+        getDataFromPreviousActivity=intent.getStringExtra("message");
+
+        Toast.makeText(this, getDataFromPreviousActivity, Toast.LENGTH_SHORT).show();
+
         listView=(ListView)findViewById(R.id.item_list_view);
+
+        onCLickListViewItem();
+        
         discoversPeersBroadcastReciever=new DiscoversPeersBroadcastReciever(this,mChannel,wifiP2pManager);
         intentFilter=new IntentFilter();
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-        discoverPeers();
+
+
+
     }
+
+
 
     @Override
     protected void onResume() {
@@ -86,6 +142,62 @@ public class DiscoverPeers extends AppCompatActivity {
 
     };
 
+    /*
+    *TO establish a connection when we click on list view item
+     */
+    public void onCLickListViewItem(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, final int positon, long l) {
+                // Picking the first device found on the network.
+                WifiP2pDevice device = deviceArray[positon];
+
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+
+
+                wifiP2pManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+
+                    @Override
+                    public void onSuccess() {
+                        // WiFiDirectBroadcastReceiver notifies us. Ignore for now.
+                        Toast.makeText(DiscoverPeers.this,"Connected to "+deviceNameArray[positon],Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        Toast.makeText(DiscoverPeers.this, "Connect failed. Retry.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    WifiP2pManager.ConnectionInfoListener connectionInfoListener=new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
+            final InetAddress groupOwnerAddress=wifiP2pInfo.groupOwnerAddress;
+            if(wifiP2pInfo.groupFormed&&wifiP2pInfo.isGroupOwner){
+                Toast.makeText(DiscoverPeers.this,"Host",Toast.LENGTH_SHORT).show();
+                listView.setVisibility(View.GONE);
+                sendTextMessage.setVisibility(View.VISIBLE);
+                serverClass=new ServerClass();
+                serverClass.start();
+
+            }
+            else{
+                Toast.makeText(DiscoverPeers.this,"Client",Toast.LENGTH_SHORT).show();
+                listView.setVisibility(View.GONE);
+                clientClass=new ClientClass(groupOwnerAddress);
+                clientClass.start();
+            }
+
+
+        }
+    };
+
+
 
 
     public void discoverPeers(){
@@ -118,4 +230,107 @@ public class DiscoverPeers extends AppCompatActivity {
             }
         });
     }
+
+    /*
+    Creates a server socket class which response back to client
+     */
+    public class ServerClass extends Thread{
+        ServerSocket serverSocket;
+        Socket socket;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket=new ServerSocket(3333);
+                socket=serverSocket.accept();
+                sendReceive=new SendReceive(socket);
+                sendReceive.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /*
+    client side class which send request to server
+     */
+    public class ClientClass extends Thread{
+        String hostAddress;
+        Socket socket;
+        public ClientClass(InetAddress hostAddres){
+            this.hostAddress=hostAddres.getHostAddress();
+            socket=new Socket();
+        }
+
+        @Override
+        public void run() {
+            try {
+                socket.connect(new InetSocketAddress(hostAddress,3333),500);
+                sendReceive=new SendReceive(socket);
+                sendReceive.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    Handler handler= new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message message) {
+            switch(message.what){
+                case MESSAGE_READ:
+                    byte[] readByte=(byte[])message.obj;
+                    String tempMsg=new String(readByte,0,message.arg1);
+                    Toast.makeText(DiscoverPeers.this, tempMsg, Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+        }
+    });
+
+    private class SendReceive extends Thread{
+        private Socket socket;
+        private InputStream inputStream;
+        private OutputStream outputStream;
+
+
+        public SendReceive(Socket sock){
+            this.socket=sock;
+            try{
+                inputStream=socket.getInputStream();
+                outputStream=socket.getOutputStream();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer=new byte[1024];
+            int bytes;
+            while(socket!=null){
+                try{
+                    bytes=inputStream.read(buffer);
+                    if(bytes>0){
+                        handler.obtainMessage(MESSAGE_READ,bytes,-1,buffer).sendToTarget();
+                    }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void write(byte[] bytes){
+            try {
+                outputStream.write(bytes);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
