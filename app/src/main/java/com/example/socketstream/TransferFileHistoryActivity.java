3848dd3ed0d17d043053efb0316b461b94b10e68
Aman <amanhack08@gmail.com>
2020-11-01 16:35:03 +0530

    add image textview

--- app/src/main/java/com/example/socketstream/TransferFileHistoryActivity.java
+++ app/src/main/java/com/example/socketstream/TransferFileHistoryActivity.java
@@ -1,15 +1,43 @@
 package com.example.socketstream;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.socketstream.transferhistory.ReceiveImages;
+import com.example.socketstream.transferhistory.ReceiveTextMessages;
+
 public class TransferFileHistoryActivity extends AppCompatActivity {
+    TextView receiveTextMessageHistory,receiveImagesHistory;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.transfer_history_activity);
+        receiveTextMessageHistory=(TextView)findViewById(R.id.receive_text_message_history);
+        receiveImagesHistory=(TextView)findViewById(R.id.receive_images_history);
+        setClickListner();
+    }
+
+    public void setClickListner(){
+        receiveTextMessageHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(TransferFileHistoryActivity.this, ReceiveTextMessages.class);
+                startActivity(intent);
+            }
+        });
+
+        receiveImagesHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(TransferFileHistoryActivity.this, ReceiveImages.class);
+                startActivity(intent);
+            }
+        });
     }
 }
