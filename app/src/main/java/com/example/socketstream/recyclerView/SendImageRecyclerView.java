c30793c373781f55174d62f9e6be53cb61457f5f
Aman <amanhack08@gmail.com>
2020-11-01 16:39:01 +0530

    add getItemCount function and checkbox selection

--- app/src/main/java/com/example/socketstream/recyclerView/SendImageRecyclerView.java
+++ app/src/main/java/com/example/socketstream/recyclerView/SendImageRecyclerView.java
@@ -1,15 +1,20 @@
 package com.example.socketstream.recyclerView;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,14 +27,19 @@ import java.util.BitSet;
 public class SendImageRecyclerView extends RecyclerView.Adapter<SendImageRecyclerView.ViewHolder>{
 
 
-    ArrayList<Bitmap> imageBitmap;
+
     Cursor mMediaStoreCursor=null;
 
-    CheckBox checkBox;
+    static ArrayList<Uri> imageUri;
+
+
+
     Activity mActivity;
 
+    //public constructor
     public SendImageRecyclerView(Activity mActivity){
         this.mActivity=mActivity;
+        imageUri=new ArrayList<>();
     }
 
     @NonNull
@@ -42,15 +52,81 @@ public class SendImageRecyclerView extends RecyclerView.Adapter<SendImageRecycle
         return new ViewHolder(view);
     }
 
+    /*
+    give value to layout widgets
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Bitmap bitmap = getBitmapFromMediaStore(position);
+        final CheckBox checkBox1=holder.getCheckBox();
+
+        //in some cases, it will prevent unwanted situations
+        //checkBox1.setOnCheckedChangeListener(null);
+        mMediaStoreCursor.moveToPosition(position);
+        int idIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns._ID);
+        int id=mMediaStoreCursor.getInt(idIndex);
+        final Uri uri= ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id);
         if (bitmap != null) {
             holder.getImageView().setImageBitmap(bitmap);
+            holder.getImageView().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                   // CheckBox checkBox=(CheckBox)view.findViewById(R.id.send_image_recycler_check_box);
+                    //TextView text=(TextView)view.findViewById(R.id.send_text_image_fragment_view);
+                    if(checkBox1.isChecked()){
+                        checkBox1.setChecked(false);
+                        for(int i=0;i<imageUri.size();i++){
+                            if(imageUri.get(i).toString().equals(uri.toString())){
+                                imageUri.remove(i);
+                                break;
+                            }
+                        }
+                        Toast.makeText(mActivity, ""+imageUri.size(), Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        checkBox1.setChecked(true);
+                        imageUri.add(uri);
+                        Toast.makeText(mActivity, ""+imageUri.size(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            checkBox1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //TextView text=(TextView)view.findViewById(R.id.send_text_image_fragment_view);
+                    if(!checkBox1.isChecked()) {
+                        checkBox1.setChecked(false);
+                        for (int i = 0; i < imageUri.size(); i++) {
+                            if (imageUri.get(i).toString().equals(uri.toString())) {
+                                imageUri.remove(i);
+                                break;
+                            }
+                        }
+                        Toast.makeText(mActivity, "" + imageUri.size(), Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        checkBox1.setChecked(true);
+                        imageUri.add(uri);
+                        Toast.makeText(mActivity, ""+imageUri.size(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
 
     }
 
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    //return the number of images
     @Override
     public int getItemCount() {
         return mMediaStoreCursor==null?0 : mMediaStoreCursor.getCount();
@@ -63,18 +139,28 @@ public class SendImageRecyclerView extends RecyclerView.Adapter<SendImageRecycle
      */
     class ViewHolder extends RecyclerView.ViewHolder{
         ImageView imageView;
+        CheckBox checkBox;
 
         public ViewHolder(@NonNull View v) {
             super(v);
             imageView=(ImageView)v.findViewById(R.id.send_image_recycler_image_view);
             checkBox=(CheckBox)v.findViewById(R.id.send_image_recycler_check_box);
         }
+        /*
+        function which return imageview
+         */
         public ImageView getImageView(){
             return  imageView;
+        }
+        public CheckBox getCheckBox(){
+            return checkBox;
         }
 
     }
 
+    /*
+    if cursor changes new value assign to mMediaStoreCursor
+     */
     private Cursor swapCursor(Cursor cursor) {
         if (mMediaStoreCursor == cursor) {
             return null;
@@ -87,6 +173,10 @@ public class SendImageRecyclerView extends RecyclerView.Adapter<SendImageRecycle
         return oldCursor;
     }
 
+    /*
+    if cursor change then we close the oldcursor
+    and assign new value to mMediaStoreCursor after calling swapCursor
+     */
     public void changeCursor(Cursor cursor) {
         Cursor oldCursor = swapCursor(cursor);
         if (oldCursor != null) {
@@ -94,6 +184,9 @@ public class SendImageRecyclerView extends RecyclerView.Adapter<SendImageRecycle
         }
     }
 
+    /*
+    return bitmap of an image which the cursor is pointed
+     */
     private Bitmap getBitmapFromMediaStore(int position) {
         int idIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns._ID);
         int mediaTypeIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE);
@@ -117,5 +210,12 @@ public class SendImageRecyclerView extends RecyclerView.Adapter<SendImageRecycle
             default:
                 return null;
         }
+    }
+
+    /*
+    function to return an ArrayList of uri
+     */
+    public static ArrayList<Uri> getArrayListUri(){
+        return imageUri;
     }
 }
