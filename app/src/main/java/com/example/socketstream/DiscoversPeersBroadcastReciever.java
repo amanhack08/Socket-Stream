66b8684ad7457317954f3c300f24d01a26b7e16c
Aman <amanhack08@gmail.com>
2020-11-02 11:21:33 +0530

    add all type of intent action

--- app/src/main/java/com/example/socketstream/DiscoversPeersBroadcastReciever.java
+++ app/src/main/java/com/example/socketstream/DiscoversPeersBroadcastReciever.java
@@ -3,6 +3,7 @@ package com.example.socketstream;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.widget.Toast;
 
@@ -34,6 +35,15 @@ public class DiscoversPeersBroadcastReciever extends BroadcastReceiver {
             }
 
         }else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
+            if(p2pManager==null)
+                return;
+            NetworkInfo networkInfo=intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+            if(networkInfo.isConnected()){
+                p2pManager.requestConnectionInfo(channel,mainActivity.connectionInfoListener);
+            }
+            else{
+                Toast.makeText(mainActivity,"It is disconnected",Toast.LENGTH_SHORT).show();
+            }
 
         }else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)){
 
